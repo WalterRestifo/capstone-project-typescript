1604c796c818638f877194d17ee434eed6fcedc5
Walter Restifo <116001970+WalterRestifo@users.noreply.github.com>
2023-01-13 16:39:00 +0100

    add a navbar (#11)

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,7 +2,13 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export default function Navigation(): JSX.Element {
-  return <StyledLink href={"/newUserForm"}>create your profile</StyledLink>;
+  return (
+    <StyledNav>
+      <StyledLink href={"/newUserForm"}>create a profile</StyledLink>
+      <StyledLink href={"/games"}>Competitions</StyledLink>
+      <StyledLink href={"/"}>Home</StyledLink>
+    </StyledNav>
+  );
 }
 
 const StyledLink = styled(Link)`
@@ -12,4 +18,10 @@ const StyledLink = styled(Link)`
   color: black;
   text-align: center;
   padding-top: 1.5rem;
+  border: 1px solid black;
+`;
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
 `;
